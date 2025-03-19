4c0b5c9aa80afceb328d743f7abfc5fd598cb397
SakshamMustFin <s.saksham@mufin.co.kr>
2025-03-19 14:17:29 +0530

    Refactor main app structure to support async initialization; integrate local storage and navigation services, and configure GoRouter for routing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
-import 'package:diamond_kgk_app/presentation/screens/filter_page.dart';
+import 'package:diamond_kgk_app/services/local_storage_service.dart';
+import 'package:diamond_kgk_app/services/navigation_service.dart';
+import 'package:diamond_kgk_app/services/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  // Ensures Flutter is fully initialized before we do async operations
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 1. Initialize Hive + Open the required box
+  await LocalStorageService.init();
+
+  // 2. Setup GetIt locator (optional, if you're using service locator)
+  setupLocator();
+
+  // 3. Run your app
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Use your shared GoRouter object
+  final GoRouter _router = appRouter;
 
-  // This widget is the root of your application.
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'KGK Diamond Selection',
+      // Provide your GoRouter configuration
+      routerConfig: _router,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true, // optional
       ),
-      home: const FilterPage(),
     );
   }
 }
